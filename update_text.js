7a8728cdd045dba861eee34f9d4a1caaad7049f8
benno <BennoCrafterDev@googlemail.com>
2023-09-04 16:13:39 +0200

    fixed bug with instant loading

--- update_text.js
+++ update_text.js
@@ -1,31 +1,32 @@
-async function updateText() {
-    // Get references to the HTML elements by their IDs
-    const weatherElement = document.getElementById("weather");
-    const timeElement = document.getElementById("current-time");
-    // Check if the elements exist
+document.addEventListener("DOMContentLoaded", function () {
+    // This code will run after the DOM has fully loaded
 
-      const newTimeData = getCurrentTime();
-      
-      weatherElement.innerText = "Weather: " + "coming soon";
-      timeElement.innerText = "Current Time: " + newTimeData;
-    
-  }
-  
+    function updateText() {
+        // Get references to the HTML elements by their IDs
+        const weatherElement = document.getElementById("weather");
+        const timeElement = document.getElementById("current-time");
+        // Check if the elements exist
 
-  function getCurrentTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, "0");
-    const minutes = now.getMinutes().toString().padStart(2, "0");
-    const seconds = now.getSeconds().toString().padStart(2, "0");
+        const newTimeData = getCurrentTime();
 
-    // Format the time in German time format (24-hour) with seconds
-    const timeString = `${hours}:${minutes}:${seconds}`;
+        weatherElement.innerText = "Weather: " + "coming soon";
+        timeElement.innerText = "Current Time: " + newTimeData;
+    }
 
-    return timeString;
-}
+    function getCurrentTime() {
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, "0");
+        const minutes = now.getMinutes().toString().padStart(2, "0");
+        const seconds = now.getSeconds().toString().padStart(2, "0");
 
+        // Format the time in German time format (24-hour) with seconds
+        const timeString = `${hours}:${minutes}:${seconds}`;
 
+        return timeString;
+    }
 
-// weather (in same file so flask server not required, but u can see it also in get_weather.js)
+    // Weather (in the same file so Flask server not required, but you can see it also in get_weather.js)
+    updateText()
+    setInterval(updateText, 1000);
 
-setInterval(updateText, 1000);
+});
