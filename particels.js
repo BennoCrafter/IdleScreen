095a5cb714ab36f905729ce541a03dd49c9df7da
benno <BennoCrafterDev@googlemail.com>
2023-09-04 15:32:05 +0200

    added base of widgets

--- particels.js
+++ particels.js
@@ -1,47 +1,45 @@
-// Number of particles
-const numParticles = 100;
+const numParticles = 150;
 
-// Function to create a particle
 function createParticle() {
-   const particle = document.createElement('div');
-   particle.className = 'particle';
-   document.querySelector('.particles-container').appendChild(particle);
-   return particle;
+    const particle = document.createElement('div');
+    particle.className = 'particle';
+    document.querySelector('.particles-container').appendChild(particle);
+    return particle;
 }
 
 // Function to animate particles
 function animateParticles() {
-   const particlesContainer = document.querySelector('.particles-container');
+    const particlesContainer = document.querySelector('.particles-container');
 
-   function animateParticle(particle) {
-       const x = Math.random() * window.innerWidth;
-       const y = Math.random() * window.innerHeight;
+    function animateParticle(particle) {
+        const x = Math.random() * window.innerWidth;
+        const y = Math.random() * window.innerHeight;
 
-       particle.style.left = `${x}px`;
-       particle.style.top = `${y}px`;
+        particle.style.left = `${x}px`;
+        particle.style.top = `${y}px`;
 
-       const size = Math.random() * 30 + 5;
-       particle.style.width = `${size}px`;
-       particle.style.height = `${size}px`;
+        const size = Math.random() * 20 + 2; // Adjust the size range as desired
+        particle.style.width = `${size}px`;
+        particle.style.height = `${size}px`;
 
-       const animationDuration = Math.random() * 3 + 2;
-       particle.style.animation = `move ${animationDuration}s linear infinite`;
+        const animationDuration = Math.random() * 3 + 2;
+        particle.style.animation = `move ${animationDuration}s linear infinite, grow ${animationDuration}s ease-in-out infinite`;
 
-       // Remove particles after a certain time to prevent memory leaks
-       setTimeout(() => {
-           particle.remove();
-           // Create a new particle to replace the removed one
-           animateParticle(createParticle());
-       }, animationDuration * 1000);
-   }
+        // Remove particles after a certain time to prevent memory leaks
+        setTimeout(() => {
+            particle.remove();
+            // Create a new particle to replace the removed one
+            animateParticle(createParticle());
+        }, animationDuration * 1000);
+    }
 
-   // Initialize particles
-   for (let i = 0; i < numParticles; i++) {
-       animateParticle(createParticle());
-   }
+    // Initialize particles
+    for (let i = 0; i < numParticles; i++) {
+        animateParticle(createParticle());
+    }
 }
 
 // Initialize animation when the DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
-   animateParticles();
+    animateParticles();
 });
